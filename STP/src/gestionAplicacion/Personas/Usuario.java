7cac513a661171b93839ab8e0c237b1335fc3889
Anarragan <111092169+Anarragan@users.noreply.github.com>
2022-10-12 20:09:51 -0500

    Usuario y Viaje

---
+++ STP/src/gestionAplicacion/Personas/Usuario.java
@@ -0,0 +1,57 @@
+package Personas;
+import Planeacion.Mercancia;
+import Planeacion.Facturacion;
+
+
+import java.util.ArrayList;
+
+public class Usuario extends Persona {
+	private ArrayList<Viaje> viaje;
+	private ArrayList<Mercancia> mercancia;
+	private ArrayList<Facturacion> facturacion;
+	
+//Usuario hereda de persona
+public Usuario(String nombre, String documento, String direccion, String edad) {
+	super();
+	this.nombre=nombre;
+	this.documento=documento;
+	this.direccion=direccion;
+	this.edad=edad;
+}
+
+//Getters and setters
+public ArrayList<Viaje> getViaje() {
+	return viaje;
+}
+
+public void setViaje(ArrayList<Viaje> viaje) {
+	this.viaje = viaje;
+}
+
+public ArrayList<Mercancia> getMercancia() {
+	return mercancia;
+}
+
+public void setMercancia(ArrayList<Mercancia> mercancia) {
+	this.mercancia = mercancia;
+}
+
+public ArrayList<Facturacion> getFacturacion() {
+	return facturacion;
+}
+
+public void setFacturacion(ArrayList<Facturacion> facturacion) {
+	this.facturacion = facturacion;
+}
+
+//Methods
+
+
+
+
+
+
+
+
+
+}
---
+++ STP/src/gestionAplicacion/Planeacion/Viaje.java
@@ -0,0 +1,84 @@
+package planeacion;
+
+import Destinos.Ciudad;
+import Personas.Conductor;
+import Personas.Usuario;
+import Destinos.Conexion;
+
+import java.util.ArrayList;
+
+public class Viaje {
+	private Ciudad cDestino="";
+	private Ciudad cOrigen="";
+	private ArrayList<Conexion> ruta;
+	private int nPasajeros=0;
+	private ArrayList<Usuario> pasajeros;
+	private ArrayList<Conductor> conductor;
+	private String fecha="";
+	
+//Getters and setters
+	public Ciudad getcDestino() {
+		return cDestino;
+	}
+
+	public void setcDestino(Ciudad cDestino) {
+		this.cDestino = cDestino;
+	}
+
+	public Ciudad getcOrigen() {
+		return cOrigen;
+	}
+
+	public void setcOrigen(Ciudad cOrigen) {
+		this.cOrigen = cOrigen;
+	}
+
+	public ArrayList<Conexion> getRuta() {
+		return ruta;
+	}
+
+	public void setRuta(ArrayList<Conexion> ruta) {
+		this.ruta = ruta;
+	}
+
+	public int getnPasajeros() {
+		return nPasajeros;
+	}
+
+	public void setnPasajeros(int nPasajeros) {
+		this.nPasajeros = nPasajeros;
+	}
+
+	public ArrayList<Usuario> getPasajeros() {
+		return pasajeros;
+	}
+
+	public void setPasajeros(ArrayList<Usuario> pasajeros) {
+		this.pasajeros = pasajeros;
+	}
+
+	public ArrayList<Conductor> getConductor() {
+		return conductor;
+	}
+
+	public void setConductor(ArrayList<Conductor> conductor) {
+		this.conductor = conductor;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	
+//methods
+	public crearViaje() {
+		
+	}
+
+	
+}
