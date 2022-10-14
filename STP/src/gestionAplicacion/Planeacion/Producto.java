98474659fc9033e17517b97a412f255d1f3b89c1
Dmoreno2903 <jaguirremo@unal.edu.co>
2022-10-13 23:37:07 -0500

    STP

---
+++ STP/src/gestionAplicacion/Planeacion/Producto.java
@@ -0,0 +1,52 @@
+package gestionAplicacion.Planeacion;
+import gestionAplicacion.Destinos.*;
+
+public class Producto {
+	private String tipo;
+	private Double peso;
+	private Ciudad cOrigen;
+	private Ciudad cDestino;
+	
+	public Producto(String tipo, Double peso, Ciudad cOrigen, Ciudad cDestino) {
+		super();
+		this.tipo = tipo;
+		this.peso = peso;
+		this.cOrigen = cOrigen;
+		this.cDestino = cDestino;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public Ciudad getcOrigen() {
+		return cOrigen;
+	}
+
+	public void setcOrigen(Ciudad cOrigen) {
+		this.cOrigen = cOrigen;
+	}
+
+	public Ciudad getcDestino() {
+		return cDestino;
+	}
+
+	public void setcDestino(Ciudad cDestino) {
+		this.cDestino = cDestino;
+	}
+	
+	
+	
+}
--- STP/src/uiMain/Principal.java
+++ STP/src/uiMain/Principal.java
@@ -4,13 +4,5 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("\n       Bienvenido a ");
-		System.out.println(" ______     ______   ______\n"
-				+ "/\\  ___\\   /\\__  _\\ /\\  == \\\n"
-				+ "\\ \\___  \\  \\/_/\\ \\/ \\ \\  _-/\n"
-				+ " \\/\\_____\\    \\ \\_\\  \\ \\_\\\n"
-				+ "  \\/_____/     \\/_/   \\/_/\n");
-		System.out.println("SISTEMA DE TRANSPORTE PERSONALIZADO");
 	}
-
 }
