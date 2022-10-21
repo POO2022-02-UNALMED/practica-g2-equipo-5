7da0855762e43601db2ec10eea6d1baa82b0053a
Alejandro Giraldo <alejandro.giraldo@artifex.co>
2022-10-20 21:36:54 -0500

    Se adiciona funcionalidad crearViaje

--- STP/src/gestionAplicacion/Planeacion/Viaje.java
+++ STP/src/gestionAplicacion/Planeacion/Viaje.java
@@ -1,20 +1,24 @@
 package gestionAplicacion.Planeacion;
 
-import gestionAplicacion.Destinos.*;
-import gestionAplicacion.Personas.*;
-
 import java.util.ArrayList;
+
+import gestionAplicacion.Destinos.Ciudad;
+import gestionAplicacion.Personas.Conductor;
+import gestionAplicacion.Personas.Usuario;
+import gestionAplicacion.Vehiculos.VehiculoPasajeros;
 
 public class Viaje {
 	private Ciudad cDestino;
 	private Ciudad cOrigen;
-	private ArrayList<Conexion> ruta = new ArrayList<Conexion> ();
+	private Ruta ruta;
 	private int nPasajeros=0;
 	private ArrayList<Usuario> pasajeros = new ArrayList<Usuario>();
 	private ArrayList<Conductor> conductor = new ArrayList<Conductor>();
 	private String fecha;
+	private VehiculoPasajeros vehiculo;
 	
-//Getters and setters
+
+	//Getters and setters
 	public Ciudad getcDestino() {
 		return cDestino;
 	}
@@ -31,11 +35,11 @@ public class Viaje {
 		this.cOrigen = cOrigen;
 	}
 
-	public ArrayList<Conexion> getRuta() {
+	public Ruta getRuta() {
 		return ruta;
 	}
 
-	public void setRuta(ArrayList<Conexion> ruta) {
+	public void setRuta(Ruta ruta) {
 		this.ruta = ruta;
 	}
 
@@ -46,17 +50,27 @@ public class Viaje {
 	public void setnPasajeros(int nPasajeros) {
 		this.nPasajeros = nPasajeros;
 	}
-
+	
+	public void aumentarNPasajeros() {
+		nPasajeros++;
+	}
+	
 	public ArrayList<Usuario> getPasajeros() {
 		return pasajeros;
 	}
-
+	public void agregarPasajeros(Usuario pasajero) {
+		this.pasajeros.add(pasajero);
+	}
 	public void setPasajeros(ArrayList<Usuario> pasajeros) {
 		this.pasajeros = pasajeros;
 	}
 
 	public ArrayList<Conductor> getConductor() {
 		return conductor;
+	}
+	
+	public void agregarconductor(Conductor conductor) {
+		this.conductor.add(conductor);
 	}
 
 	public void setConductor(ArrayList<Conductor> conductor) {
@@ -69,5 +83,13 @@ public class Viaje {
 
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
+	}
+	
+	public VehiculoPasajeros getVehiculo() {
+		return vehiculo;
+	}
+	
+	public void setVehiculo(VehiculoPasajeros vehiculo) {
+		this.vehiculo = vehiculo;
 	}
 }
